ef1f4aac994bf969194b8970e6a1787af3b104eb
DarkCthulhu <anirudh4444@gmail.com>
2013-08-17 23:54:58 +0530

    translation done

--- Flies/js/main_game.js
+++ Flies/js/main_game.js
@@ -1,6 +1,6 @@
 //content globals
-var CANVAS_WIDTH = $(window).width() - 30;
-var CANVAS_HEIGHT = $(window).height() - 30;
+var CANVAS_WIDTH;
+var CANVAS_HEIGHT;
 var FPF = 60 //frames per fly :D
 var MAX_SCREEN_FLIES = 2;
 var SCREEN_TIME = 2;
@@ -13,12 +13,14 @@ var allowance = 20;
 var fCount = 0; //number of frames completed by flies in current position
 
 $(function(){
-	var canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
+	CANVAS_WIDTH = document.body.clientWidth;
+    CANVAS_HEIGHT = document.body.clientHeight;
+    
+    var canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
                       "' height='" + CANVAS_HEIGHT + "'></ctx>");
 	
 	ctx = canvasElement.get(0).getContext("2d");
-	canvasElement.appendTo('body');	
-    
+    canvasElement.appendTo('body');	
     //create game objects
     createElements();
     
@@ -44,24 +46,24 @@ $(function(){
 });
 
 
+//draws fingers detected by the leap-motion sensor
 function drawFingers(args) {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     
     //update and draw the objects on screen
     update();
     draw();
-
+    
     //find the fingers on screen
     var pointablesMap = args.pointablesMap;
         for (var i in pointablesMap) {
         var pointable = pointablesMap[i];
         var pos = pointable.tipPosition;
-
+        
         var radius = Math.min(600/Math.abs(pos[2]),20);
         ctx.beginPath();
-        ctx.arc(pos[0]*2,-pos[1]*2,radius,0,2*Math.PI);
+        ctx.arc(pos[0]*4 + CANVAS_WIDTH/3.5,-pos[1]*4 + CANVAS_HEIGHT*1.9,radius,0,2*Math.PI);
         ctx.fill();
-        writeHeader(pos[0] + " -- " + pos[1]);
     }
 };
 
@@ -70,12 +72,15 @@ function writeHeader(value){
     $("#connection_state").html(value);
 }
 
+//creates the flies
 function createElements(){
     for(var i=0;i<MAX_SCREEN_FLIES;i++){
         var player = new Fly();
         playerArray.push(player);
     }
 }
+
+//updates fly position
 function update(){
     if(fCount < FPF){
         fCount++;
@@ -83,16 +88,19 @@ function update(){
         fCount = 0;
         for(var i=0;i<MAX_SCREEN_FLIES;i++){
             var player = playerArray[i];
-            player.setPosition(getRandom(CANVAS_WIDTH - allowance), getRandom(CANVAS_HEIGHT - allowance));
+            player.setPosition(getRandom(CANVAS_WIDTH), getRandom(CANVAS_HEIGHT));
         }
     }
 }
+
+//draws flies to screen
 function draw() {
     for(var i=0;i<MAX_SCREEN_FLIES;i++){
         var player = playerArray[i];
         player.draw();
     }
 }
+
 function getRandom(limit){
     return Math.floor((Math.random()*limit)+1);
 }
